7ca00d8e66ce6a4f6024363f5b7119cb08a51656
Ritik Srivastava <ritik.space@gmail.com>
2023-12-24 02:21:30 +0530

    ✨ Add FontAwesome Support

---
+++ src/app/components/icon/icon.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { unicodeToString } from '../../../utils/string';
+
+@Component({
+  selector: 'app-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.scss'
+})
+export class IconComponent {
+  @Input() code: string = '';
+  @Input() type?: keyof typeof EIconType = 'light';
+  @Output() onClick = new EventEmitter();
+
+  constructor() {}
+
+  getUnicode() {
+		return unicodeToString(this.code)
+	}
+
+	onClickEvent() {
+		this.onClick.emit();
+	}
+}
+
+export enum EIconType {
+	thin = 'thin',
+	light = 'light',
+	regular = 'regular',
+	solid = 'solid',
+	duotone = 'duotone',
+	sharpSolid = 'sharpSolid',
+	sharpRegular = 'sharpRegular',
+	sharpLight = 'sharpLight',
+	brand = 'brand',
+}
