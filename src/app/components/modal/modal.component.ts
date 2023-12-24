bc3d8a754d71d50f299943dbb1bc87ab2e09ad61
Ritik Srivastava <ritik.space@gmail.com>
2023-12-25 00:01:49 +0530

    ✨ New Sidebar Modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-
+  @Input() title?: string = '';
   @Output() onClose = new EventEmitter();
   onCloseEvent() {
 		this.onClose.emit();
